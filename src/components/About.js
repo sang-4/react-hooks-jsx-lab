2bd9dc278f229707b922d37793592cd1f7cb1a28
Allan <kipropsallan@gmail.com>
2022-07-13 15:13:56 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; 
+
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am a Software Engineering student at Moringa School, aspiring to be a Fullstack JavaScript Developer. Proficient in HTML, CSS, JavaScript and some bit of React</p>
+    <img src={image} alt="I made this"/>
+  </div>
 }
 
 export default About;
--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Allan";
+const city = "Nairobi";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const image = "https://media-exp1.licdn.com/dms/image/C4D03AQGfeMLyVAhZyQ/profile-displayphoto-shrink_800_800/0/1618053500634?e=1663200000&v=beta&t=cYrrI0-dh7G5kXAnF3-rywJ-J5sM4-pjc4cUk37FedI";
 
 export { name, city, image };
